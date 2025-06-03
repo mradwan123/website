b3f6bf16dd1a5b13e0a2b8857e6338543b1671d4
mradwan123 <radwan@duck.com>
2025-06-03 10:14:45 +0200

    adding all files

---
+++ script.js
@@ -0,0 +1,9 @@
+
+  function myFunction() {
+    const links = document.getElementById("myLinks");
+    if (links.style.display === "block") {
+      links.style.display = "none";
+    } else {
+      links.style.display = "block";
+    }
+  }
